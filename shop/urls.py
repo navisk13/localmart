488a725badcb2f7daee3d9b5b633d2a70f70a38e
Navaneeth <navisk13@gmail.com>
2020-09-22 14:42:03 +0530

    small updates

--- shop/urls.py
+++ shop/urls.py
@@ -9,14 +9,14 @@ from django.contrib.auth.decorators import login_required
 app_name = 'shop'
 
 urlpatterns = [
-    url(r'^accounts/login/$', auth_views.login, name="login"),
-    url(r'^logout/$', auth_views.logout, name="logout"),
-    url(r'^password_change/$', auth_views.password_change, name="password_change_form"),
-    url(r'^changepassword/done/$', auth_views.password_change_done, name="password_change_done"),
-    url(r'^resetpassword/$', auth_views.password_reset, name="password_reset_form"),
-    url(r'^resetpassword/confirm/$', auth_views.password_reset_confirm, name="password_reset_confirm"),
-    url(r'^resetpassword/complete/$', auth_views.password_reset_complete, name="password_reset_complete"),
-    url(r'^resetpassword/done/$', auth_views.password_reset_done, name="password_reset_done"),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name="login"),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name="logout"),
+    url(r'^password_change/$', auth_views.PasswordChangeView.as_view(), name="password_change_form"),
+    url(r'^changepassword/done/$', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    url(r'^resetpassword/$', auth_views.PasswordResetView.as_view(), name="password_reset_form"),
+    url(r'^resetpassword/confirm/$', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    url(r'^resetpassword/complete/$', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    url(r'^resetpassword/done/$', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     url(r'^signup/$',views.signup, name="signup"),
     url(r'^profile/$', views.profiledetailview, name="profile"),
     url(r'^profile/delete/$', ConfirmDelete.as_view(), name="profile_delete"),
